2d7136f800a984b0215762255aa8e73917f04c62
gusvarg <gvargasrub@gmail.com>
2025-11-23 10:38:12 -0500

    Update woocommerce.ts

--- types/woocommerce.ts
+++ types/woocommerce.ts
@@ -19,11 +19,21 @@ export interface Product {
   images: ProductImage[];
   attributes: ProductAttribute[];
   price_html?: string;
+  meta_data?: MetaData[];
+  categories?: Category[];
+  tags?: Tag[];
+  short_description?: string;
+  description?: string;
+  price?: string;
+  regular_price?: string;
+  sale_price?: string;
+  stock_status?: string;
 }
 
 export interface VariationAttribute {
   name: string; // "Color"/"Tallas" o prefijado "pa_..."
   option: string;
+  slug?: string;
 }
 
 export interface Variation {
@@ -31,4 +41,43 @@ export interface Variation {
   price?: string;
   image?: ProductImage | null;
   attributes: VariationAttribute[];
+  meta_data?: MetaData[];
+}
+
+export interface MetaData {
+  id: number;
+  key: string;
+  value: unknown;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  slug: string;
+  parent: number;
+  description?: string;
+  image?: ProductImage | null;
+  count?: number;
+}
+
+export interface Tag {
+  id: number;
+  name: string;
+  slug: string;
+  description?: string;
+  count?: number;
+}
+
+export interface AttributeTerm {
+  id: number;
+  name: string;
+  slug: string;
+  description?: string;
+  menu_order?: number;
+  count?: number;
+}
+
+export interface AttributeWithTerms {
+  attribute: ProductAttribute;
+  terms: AttributeTerm[];
 }
